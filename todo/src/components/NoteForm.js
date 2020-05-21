65c1c19d81e4a837ea6ad12060d4f7181e22bfdc
Taja House <ahlliahtaja@yahoo.com>
2020-05-21 14:20:55 -0700

    Completed MVP

--- todo/src/components/NoteForm.js
+++ todo/src/components/NoteForm.js
@@ -1,33 +1,47 @@
-import React, { useState, useReducer } from 'react';
-import { initialNoteState, noteReducer} from "../reducers/reducer";
-import Notes from '../components/Notes';
+import React, { useState, useReducer } from 'react'
+import { initialNoteState, noteReducer, TOGGLE_TODO } from '../reducers/reducer'
+import Notes from '../components/Notes'
 
-const NoteForm = () =>{
-    const [noteState, dispatch] = useReducer(noteReducer, initialNoteState);
-    const [newNoteState, setNewNoteState] = useState("");
-    console.log(initialNoteState);
-    const handleChanges = e =>{
-        setNewNoteState(e.target.value)
+const NoteForm = () => {
+	const [ noteState, dispatch ] = useReducer(noteReducer, initialNoteState)
+	const [ newNoteState, setNewNoteState ] = useState('')
+	const handleChanges = (e) => {
+		setNewNoteState(e.target.value)
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+        dispatch({ type: 'ADD_TODO', note: newNoteState })
+        setNewNoteState("")
+	}
+
+	const handleClick = (e) => {
+		// props.TOGGLE_TODO(props.note.id)
+		dispatch({ type: 'TOGGLE_TODO', id: e.target.id })
+    }
+    
+    const handleDelete = e =>{
+        dispatch({type: 'DELETE_COMPLETED'})
     }
 
-    const handleSubmit = e =>{
-        e.preventDefault();
-        dispatch({ type: "ADD_TODO", note: newNoteState })
-    }
-
-    return (
-        <div>
-            <input
-            type="text"
-            name="entry"
-            placeholder="Enter Note Here"
-            value={newNoteState}
-            onChange={handleChanges}
-            />
-            <button onClick={handleSubmit} type="submit">Add Note</button>
-
-            {noteState.map(note => <Notes note={note} />)}
-        </div>
-    )
+	console.log('noteState from noteForm', noteState)
+	return (
+		<div className="note_form">
+			<input
+				type="text"
+				name="entry"
+				placeholder="Enter Note Here"
+				value={newNoteState}
+				onChange={handleChanges}
+			/>
+			<button onClick={handleSubmit} type="submit">
+				Add Note
+			</button>
+            <button onClick={handleDelete}>
+                Clear
+            </button>
+			<div className="note_wrapper">{noteState.map((note) => <Notes note={note} handleClick={handleClick} />)}</div>
+		</div>
+	)
 }
-export default NoteForm;
+export default NoteForm
--- todo/src/components/Notes.js
+++ todo/src/components/Notes.js
@@ -1,11 +1,20 @@
 import React, { useReducer } from 'react'
-import { initialNoteState, noteReducer} from "../reducers/reducer";
+import { initialNoteState, noteReducer} from "../reducers/reducer"
 
 const Notes = (props) => {
-    console.log(props);
+    const [noteState, dispatch] = useReducer(noteReducer, initialNoteState);
+
+    // const handleClick = e =>{
+    //     // props.TOGGLE_TODO(props.note.id)
+    //     dispatch({type: 'TOGGLE_TODO', id: e.target.id})
+    // }
+    console.log('props from Notes', props);
     return (
-        <div>
-            <p>{props.note.item}</p>
+        <div
+            className={`${props.note.completed ? "note completed" : "note"}`}
+        >
+            <p id={props.note.id} onClick={e => props.handleClick(e)}>{props.note.item}</p>
+            
         </div>
     )
 }
